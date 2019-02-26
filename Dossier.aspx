c7484193a7639bdce4133a91d045e988a02afef7
bumdev <kosonischenko@gmail.com>
2019-02-26 14:59:01 +0300

    page employees, dossier, new logo

---
+++ Dossier.aspx
@@ -0,0 +1,104 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPageSingleMenu.Master" AutoEventWireup="true" CodeBehind="Dossier.aspx.cs" Inherits="akademik_app.Dossier" %>
+<%@ Register TagPrefix="telerik" Namespace="Telerik.Web.UI" Assembly="Telerik.Web.UI" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
+    <link href="styles/grid.css" rel="stylesheet" />
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+  
+</asp:Content>
+
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder2" runat="Server">
+    <asp:UpdatePanel runat="server" ID="up1">
+<ContentTemplate>
+    <script src="scripts/openimage.js" type="text/javascript" ></script>     
+<div style="width:100%; border:0px solid black; min-height:300px;">
+<div>
+   
+    
+  <telerik:RadGrid runat="server" ID="radgrid" AutoGenerateColumns="false"   PageSize="25"
+        onneeddatasource="radgridDevice_NeedDataSource" CssClass="rad" AllowPaging="True"  Font-Names="Arial Unicode MS"
+        Culture="ru-RU" oninsertcommand="radgrid_InsertCommand" ClientEvents-OnRequestStart="onRequestStart"
+        AllowAutomaticInserts="true" ondeletecommand="radgrid_DeleteCommand" onupdatecommand="radgridServices_UpdateCommand"
+        onitemdatabound="radgrid_ItemDataBound" OnItemCommand="radgrid_itemcommand"
+        AllowSorting="true" RenderMode="Auto">
+      
+      <ClientSettings Selecting-AllowRowSelect="true" EnablePostBackOnRowClick="true" >
+         
+      </ClientSettings>
+
+         <ExportSettings ExportOnlyData="true" IgnorePaging="true">
+    <Pdf DefaultFontFamily="Arial Unicode MS" PageTopMargin="2" PageLeftMargin="2" PageRightMargin="2" PageBottomMargin="2"/>
+    </ExportSettings>
+      
+        <MasterTableView EditMode="PopUp" IsFilterItemExpanded="True" EditFormSettings-PopUpSettings-Width="900px" DataKeyNames="ID" Width="100%" CommandItemDisplay="Top" AllowAutomaticUpdates="true" Name="Dossier"  AllowFilteringByColumn="true" >
+            <CommandItemSettings ShowAddNewRecordButton="false" ShowRefreshButton="False" />
+            
+            <Columns>
+                <%--<telerik:GridEditCommandColumn UniqueName="EditCommandColumn" ButtonType="ImageButton"></telerik:GridEditCommandColumn>--%> 
+                
+                <telerik:GridDropDownColumn UniqueName="ep" HeaderStyle-HorizontalAlign="Center" DropDownControlType="RadComboBox" DataField="employee_id" HeaderText="Сотрудник" DataSourceID="dsEmployees" 
+                    ListTextField="name" FilterControlWidth="300" ListValueField="id" ItemStyle-Wrap="true" ItemStyle-Width="300" HeaderStyle-Width="300">
+                    <FilterTemplate>
+                        <telerik:RadComboBox runat="server"  Width="250px" ID="employee_id" DataSourceID="dsEmployees" DataTextField="name"
+                            DataValueField="id" AppendDataBoundItems="true" SelectedValue='<%# ((GridItem)Container).OwnerTableView.GetColumn("ep").CurrentFilterValue %>'
+                            runat="server" OnClientSelectedIndexChanged="DirectEmployees" Filter="Contains"
+                            EmptyMessage="Поиск...">
+                            <Items>
+                                <telerik:RadComboBoxItem />
+                            </Items>
+                        </telerik:RadComboBox>
+                        <telerik:RadScriptBlock ID="RadScriptBlock1" runat="server">
+                            <script type="text/javascript">
+                                function DirectEmployees(sender, args) {
+                                    var tableView = $find("<%# ((GridItem)Container).OwnerTableView.ClientID %>");
+                                    tableView.filter("ep", args.get_item().get_value(), "EqualTo");
+                                }
+                            </script>
+                        </telerik:RadScriptBlock>
+                    </FilterTemplate>
+                </telerik:GridDropDownColumn>
+                <%--<telerik:GridBoundColumn DataField="name" runat="server" HeaderText="ФИО" AllowFiltering="false"></telerik:GridBoundColumn>--%>
+                <%--<telerik:GridBoundColumn DataField="sex" runat="server" HeaderText="Пол"  AllowFiltering="false"></telerik:GridBoundColumn>--%>
+                <telerik:GridBoundColumn DataField="position" runat="server" HeaderText="Должность"  AllowFiltering="false"></telerik:GridBoundColumn>
+                <%--<telerik:GridBoundColumn DataField="phone" runat="server" HeaderText="Телефон"  AllowFiltering="false"></telerik:GridBoundColumn>--%>
+                <%--<telerik:GridBoundColumn DataField="growth" runat="server" HeaderText="Рост"  AllowFiltering="false"></telerik:GridBoundColumn>--%>
+                <telerik:GridBoundColumn DataField="comment" runat="server" HeaderText="Комментарии"  AllowFiltering="false"></telerik:GridBoundColumn>
+                <telerik:GridBoundColumn DataField="projects" runat="server" HeaderText="Проекты"  AllowFiltering="false"></telerik:GridBoundColumn>
+                <%--<telerik:GridDateTimeColumn CurrentFilterFunction="EqualTo" ShowFilterIcon="false" FilterControlWidth="170" AutoPostBackOnFilter="true" DataField="birthday" ReadOnly="true" HeaderText="День рождения" />--%>
+                <%--<telerik:GridBoundColumn DataField="Address" runat="server" HeaderText="Адрес"  AllowFiltering="false"></telerik:GridBoundColumn>--%>  
+                <%--<telerik:GridBoundColumn DataField="par_data" runat="server" HeaderText="Итог" AllowFiltering="false" ></telerik:GridBoundColumn>--%>
+                
+                <%--<telerik:GridNumericColumn DataField="total_ratio" runat="server"  HeaderText="Итог" AllowFiltering="false"></telerik:GridNumericColumn>--%>
+                
+                
+
+                <%--<telerik:GridButtonColumn runat="server" CommandName="Delete" Text="Delete" ButtonType="ImageButton" ConfirmText="Вы точно хотите удалить?" />--%>
+                
+        </Columns>
+        <EditFormSettings>
+                <EditColumn ButtonType="ImageButton" />            
+        </EditFormSettings>
+        
+
+        </MasterTableView>
+     
+</telerik:RadGrid>
+    
+        
+
+<asp:SqlDataSource ID="dsEmployees" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionInfo %>" SelectCommand="RetrieveEmployees"></asp:SqlDataSource>
+<%--<asp:SqlDataSource ID="dsSensors" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionInfo %>" SelectCommand="RetrieveSensors"></asp:SqlDataSource>--%>
+
+
+<asp:SqlDataSource ID="dsJournal" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionInfo %>" 
+    SelectCommand="RetrieveDossier" SelectCommandType="StoredProcedure" 
+    >
+</asp:SqlDataSource>
+</div>
+</ContentTemplate>
+</asp:UpdatePanel>
+</asp:Content>
+
+
---
+++ Dossier.aspx.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Telerik.Web.UI;
+using Entities;
+
+namespace akademik_app
+{
+    public partial class Dossier: ULPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                CheckLogin();
+            }
+
+        }
+
+
+        public bool IsEdit()
+        {
+            bool ok = false;
+            User u = GetCurrentUser();
+            u.GetPermissions();
+
+            return ok;
+
+        }
+        private void CheckLogin()
+        {
+            if (!IsLogin())
+            {
+                Response.Redirect("Home.aspx");
+            }
+
+        }
+
+        protected void radgrid_ItemDataBound(object sender, GridItemEventArgs e)
+        {
+            if ((e.Item is GridEditFormItem) && (e.Item.IsInEditMode))
+            {
+                GridEditFormItem editItem = (GridEditFormItem)e.Item;
+                RadComboBox rc = (RadComboBox)editItem["ep"].Controls[0];
+                rc.Width = Unit.Pixel(400);
+                rc.Filter = RadComboBoxFilter.Contains;
+            }
+            if (e.Item is GridDataItem)
+            {
+                GridDataItem item = (GridDataItem)e.Item;
+                //TextBox param_data = (TextBox) item["param_data"].Controls[0];
+                //if (param_data.Text == "0" || param_data.Text == null)
+                //{
+                //    TableCell tc = (TableCell) item["param_data"];
+                //    tc.BackColor = Color.Red;
+                //}
+                //GridNumericColumn param_data = (GridNumericColumn) item["param_data"].Controls[0];
+                //TextBox param_data = (TextBox) item["param_data"].Controls[0];
+                //TextBox ratio = (TextBox) item["ratio"].Controls[0];
+                //param_data.Text = Convert.ToDouble(param_data.Text/ratio.Text);
+            }
+        }
+
+        protected void radgridDevice_NeedDataSource(object sender, GridNeedDataSourceEventArgs e)
+        {
+            if (!e.IsFromDetailTable)
+            {
+                radgrid.DataSource = (DataView)dsJournal.Select(DataSourceSelectArguments.Empty);
+            }
+        }
+        protected void radgrid_InsertCommand(object sender, GridCommandEventArgs e)
+        {
+            GridEditFormInsertItem insertedItem = (GridEditFormInsertItem)e.Item;
+
+            if (e.Item.OwnerTableView.Name == "Archive")
+            {
+                Entities.User u = GetCurrentUser();
+
+                dsJournal.InsertParameters.Add(new Parameter("RegNumber", DbType.String, (insertedItem["RegNumber"].Controls[0] as TextBox).Text));
+                dsJournal.InsertParameters.Add(new Parameter("Correspondent", DbType.String, (insertedItem["Correspondent"].Controls[0] as TextBox).Text));
+                dsJournal.InsertParameters.Add(new Parameter("Address", DbType.String, (insertedItem["Address"].Controls[0] as TextBox).Text));
+                dsJournal.InsertParameters.Add(new Parameter("RegNumberJuridical", DbType.String, (insertedItem["RegNumberJuridical"].Controls[0] as TextBox).Text));
+                dsJournal.InsertParameters.Add(new Parameter("DateJuridical", DbType.DateTime, (insertedItem["date_juridical"].Controls[0] as RadDatePicker).SelectedDate.ToString()));
+                dsJournal.InsertParameters.Add(new Parameter("CodeFrom", DbType.String, (insertedItem["CodeFrom"].Controls[0] as TextBox).Text));
+                dsJournal.InsertParameters.Add(new Parameter("PlaneDate", DbType.DateTime, (insertedItem["date_plane"].Controls[0] as RadDatePicker).SelectedDate.ToString()));
+                dsJournal.InsertParameters.Add(new Parameter("DateExecution", DbType.DateTime, (insertedItem["date_exec"].Controls[0] as RadDatePicker).SelectedDate.ToString()));
+                dsJournal.InsertParameters.Add(new Parameter("TypeLetter", DbType.String, (insertedItem["TypeLetter"].Controls[0] as TextBox).Text));
+                dsJournal.InsertParameters.Add(new Parameter("TextResolution", DbType.String, (insertedItem["TextResolution"].Controls[0] as TextBox).Text));
+                dsJournal.InsertParameters.Add(new Parameter("DateView", DbType.DateTime, (insertedItem["date_view"].Controls[0] as RadDatePicker).SelectedDate.ToString()));
+                dsJournal.InsertParameters.Add(new Parameter("Viewer", DbType.String, (insertedItem["Viewer"].Controls[0] as TextBox).Text));
+                dsJournal.InsertParameters.Add(new Parameter("RegNumberOut", DbType.String, (insertedItem["RegNumberOut"].Controls[0] as TextBox).Text));
+                dsJournal.InsertParameters.Add(new Parameter("TypeMail", DbType.String, (insertedItem["TypeMail"].Controls[0] as TextBox).Text));
+                dsJournal.InsertParameters.Add(new Parameter("Comment", DbType.String, (insertedItem["Comment"].Controls[0] as TextBox).Text));
+                dsJournal.InsertParameters.Add(new Parameter("ResponsibleContractorID", DbType.Int32, (insertedItem["rc"].Controls[0] as RadComboBox).SelectedValue));
+
+
+                dsJournal.Insert();
+            }
+            e.Item.Edit = false;
+            e.Canceled = true;
+            radgrid.Rebind();
+        }
+
+        protected void radgrid_DeleteCommand(object sender, GridCommandEventArgs e)
+        {
+            if (e.Item.OwnerTableView.Name == "Archive")
+            {
+                string s = e.Item.OwnerTableView.DataKeyValues[e.Item.ItemIndex]["ID"].ToString();
+                dsJournal.DeleteParameters.Add(new Parameter("ID", DbType.Int32, s));
+                dsJournal.Delete();
+            }
+
+
+            e.Canceled = false;
+        }
+
+
+        protected void radgridServices_UpdateCommand(object sender, GridCommandEventArgs e)
+        {
+            GridEditableItem updatedItem = (GridEditableItem)e.Item;
+            if (e.Item.OwnerTableView.Name == "Archive")
+            {
+                string s = e.Item.OwnerTableView.DataKeyValues[e.Item.ItemIndex]["ID"].ToString();
+
+                dsJournal.UpdateParameters.Add(new Parameter("ID", DbType.Int32, s));
+
+
+                dsJournal.UpdateParameters.Add(new Parameter("RegNumber", DbType.String, (updatedItem["RegNumber"].Controls[0] as TextBox).Text));
+                dsJournal.UpdateParameters.Add(new Parameter("DateRegistration", DbType.DateTime, (updatedItem["date_reg"].Controls[0] as RadDatePicker).SelectedDate.ToString()));
+                dsJournal.UpdateParameters.Add(new Parameter("Correspondent", DbType.String, (updatedItem["Correspondent"].Controls[0] as TextBox).Text));
+                dsJournal.UpdateParameters.Add(new Parameter("Address", DbType.String, (updatedItem["Address"].Controls[0] as TextBox).Text));
+                dsJournal.UpdateParameters.Add(new Parameter("RegNumberJuridical", DbType.String, (updatedItem["RegNumberJuridical"].Controls[0] as TextBox).Text));
+                dsJournal.UpdateParameters.Add(new Parameter("DateJuridical", DbType.DateTime, (updatedItem["date_juridical"].Controls[0] as RadDatePicker).SelectedDate.ToString()));
+                dsJournal.UpdateParameters.Add(new Parameter("CodeFrom", DbType.String, (updatedItem["CodeFrom"].Controls[0] as TextBox).Text));
+                dsJournal.UpdateParameters.Add(new Parameter("PlaneDate", DbType.DateTime, (updatedItem["date_plane"].Controls[0] as RadDatePicker).SelectedDate.ToString()));
+                dsJournal.UpdateParameters.Add(new Parameter("DateExecution", DbType.DateTime, (updatedItem["date_exec"].Controls[0] as RadDatePicker).SelectedDate.ToString()));
+                dsJournal.UpdateParameters.Add(new Parameter("TypeLetter", DbType.String, (updatedItem["TypeLetter"].Controls[0] as TextBox).Text));
+                dsJournal.UpdateParameters.Add(new Parameter("TextResolution", DbType.String, (updatedItem["TextResolution"].Controls[0] as TextBox).Text));
+                dsJournal.UpdateParameters.Add(new Parameter("DateView", DbType.DateTime, (updatedItem["date_view"].Controls[0] as RadDatePicker).SelectedDate.ToString()));
+                dsJournal.UpdateParameters.Add(new Parameter("Viewer", DbType.String, (updatedItem["Viewer"].Controls[0] as TextBox).Text));
+                dsJournal.UpdateParameters.Add(new Parameter("RegNumberOut", DbType.String, (updatedItem["RegNumberOut"].Controls[0] as TextBox).Text));
+                dsJournal.UpdateParameters.Add(new Parameter("TypeMail", DbType.String, (updatedItem["TypeMail"].Controls[0] as TextBox).Text));
+                dsJournal.UpdateParameters.Add(new Parameter("Comment", DbType.String, (updatedItem["Comment"].Controls[0] as TextBox).Text));
+                dsJournal.UpdateParameters.Add(new Parameter("ResponsibleContractorID", DbType.Int32, (updatedItem["rc"].Controls[0] as RadComboBox).SelectedValue));
+
+                dsJournal.Update();
+
+            }
+            e.Item.Edit = false;
+            radgrid.Rebind();
+        }
+
+
+        protected void radgrid_itemcommand(object sender, GridCommandEventArgs e)
+        {
+
+        }
+    }
+}
